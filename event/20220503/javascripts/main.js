6987801fca054a228e19556aa75908edc12efe86
yoshy-m <>
2022-03-29 20:42:18 +0900

    WildNatureCamp 追加

---
+++ event/20220503/javascripts/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  $(window).scroll(function () {
+    const windowHeight = $(window).height();
+    const scroll = $(window).scrollTop();
+
+    $('.element').each(function () {
+      const targetPosition = $(this).offset().top;
+      if (scroll > targetPosition - windowHeight + 100) {
+        $(this).addClass("is-fadein");
+      }
+    });
+  });
+});
